ebb75b8ee7ec634e13e2a5c8af1ae42776d990a3
Rustin <rustinlewis@gmail.com>
2018-07-02 20:05:54 -0600

    add html file, add css folder, imported js and css into html

---
+++ js/encryptor.js
@@ -0,0 +1,17 @@
+let message = "";
+
+let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+    "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+console.log(`At index 1: ${message.charAt(0)}`);
+
+console.log(`They chose: ${message}`);
+
+function letterChange() {
+    message.replace(message.charAt(0));
+    message.replace(message.charAt(1));
+    message.replace(message.charAt(2));
+}
+            
+            
+            
